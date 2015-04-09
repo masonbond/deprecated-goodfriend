51f7d05fba1cf7394a0dd59b9db3aba3b44d0eb1
Mason Bond <mason.bond@brandedholdings.com>
2015-04-09 14:09:17 -0400

    No really, initial commit

---
+++ src/gf.core.js
@@ -0,0 +1,209 @@
+;var GF=(function(){'use strict';
+	var _public,
+		_static = {
+			nextContextId: 0,
+			contexts: [],
+			listeners: {}
+		},
+		gl,
+		shaderTypeMap;
+
+	// TODO finish refactor
+	// -	fix ur object literal initializer getter/setters
+	// -	convert ShaderProgram to have getters/setters for vert/frag and link method, like Shader
+	// -	convert context to have getter/setter for active shaderprogram
+	// -	general cleanup pass
+
+	// helpers
+
+	function raiseEvent(type) {
+		var i,
+			listeners = _static.listeners[type],
+			l;
+
+		if (!(listeners && listeners.length > 0)) return false;
+
+		l = listeners.length;
+
+		for (i = 0; i < l; ++i) {
+			listeners[i]();
+		}
+	}
+
+	_public = {
+
+		// helper constructors
+
+		Stack: function(args) {
+			// TODO Stack
+		},
+		
+		// graphics constructors
+
+		Context: function(canvas) {
+			var gl = canvas && canvas.getContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')),
+				shaderProgram = 0,
+				context,
+				id = _static.nextContextId++,
+				active = {
+					shaderProgram: 0,
+					vertexShader: 0,
+					fragmentShader: 0
+				};
+
+			// TODO log context creation failed
+			if (!gl) return false;
+
+			// map out shader types to enumerated webgl types, but only once
+			shaderTypeMap = shaderTypeMap || {
+				'x-shader/x-vertex': gl.VERTEX_SHADER,
+				'x-shader/x-fragment': gl.FRAGMENT_SHADER
+			};
+
+			raiseEvent(_public.INIT);
+
+			return context = {
+				get id() { return id; },
+				get gl() { return gl; },
+
+				get shaderProgram() { return shaderProgram; },
+				set shaderProgram(program) {
+					shaderProgram = program;
+
+					if (shaderProgram === 0 || (typeof shaderProgram === 'object' && shaderProgram.object)) {
+						gl.useProgram(shaderProgram.object);
+					}
+
+					return program;
+				},
+
+				// context helper object constructors
+
+				Shader: function(source, type) {
+					// load a glsl shader.
+					// script parameter can be an HTMLScriptElement, the object of an HTMLScriptElement, or raw GLSL source.
+					var glsl,
+						shaderType,
+						object,
+						shader;
+
+					if (type) shaderType = shaderTypeMap[type];
+
+					shader = {
+						get object() { return object; },
+						get type() { return shaderType; },
+						
+						get source() { return glsl; },
+						set source(source) {
+							var sourceIsString = (typeof source === 'string'),
+								script = (sourceIsString && document.getElementById(source)) || source;
+
+							if (script instanceof HTMLScriptElement) {
+								// script element node or object was passed in
+								shaderType = shaderTypeMap[script.type];
+								glsl = script.textContent;
+							} else if (sourceIsString) {
+								// parameter is probably raw glsl or something, whatever
+								glsl = source;
+							}
+
+							return glsl;
+						},
+						
+						compile: function() {
+							if (!glsl) return false;
+
+							// if necessary we can try to autodetect the shader type, but it's not optimal or future-proof
+							shaderType = shaderType || (/\bgl_Position/.test(glsl) ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER);
+							
+							object = gl.createShader(shaderType);
+
+							gl.shaderSource(object, glsl);
+							gl.compileShader(object);
+
+							if (!gl.getShaderParameter(object, gl.COMPILE_STATUS)) {
+								console.error(gl.getShaderInfoLog(object));
+								return false;
+							}
+
+							// we made it!
+							return true;
+						}
+					};
+
+					shader.source = source;
+
+					if (source) sshader.compile();
+					
+					return shader;
+				},
+				ShaderProgram: function(vertexShader, fragmentShader) {
+					// create a shader program.
+					var object = gl.createProgram(),
+						vert = vertexShader,
+						frag = fragmentShader,
+						program;
+
+					program = {
+						get object() { return object; },
+
+						get vertexShader() { return vert; },
+						set vertexShader(shader) { return vert = shader; },
+
+						get fragmentShader() { return frag; },
+						set fragmentShader(shader) { return frag = shader; },
+
+						link: function() {
+							if (!(vert && vert.object && frag && frag.object)) return false;
+
+							gl.attachShader(object, vert.object);
+							gl.attachShader(object, frag.object);
+							gl.linkProgram(object);
+
+							if (!gl.getProgramParameter(object, gl.LINK_STATUS)) {
+								// TODO link error
+								return false;
+							}
+
+							return true;
+						}
+					};
+
+					program.vertexShader = vertexShader;
+					program.fragmentShader = fragmentShader;
+
+					if (vertexShader && fragmentShader) program.link();
+
+					return program;
+				}
+			};
+		},
+
+		// methods
+
+		addEventListener: function(event, callback, prepend) {
+			(_static.listeners[event] = _static.listeners[event] || [])[prepend ? 'unshift' : 'push'](callback);
+		},
+		removeEventListener: function(event, callback) {
+			// remove a GF event listener with a specific callback.
+			// if callback is omitted or falsy, remove all listeners for that event
+			var listeners =  _static.listeners[event],
+				i;
+
+			if (!listeners) return false;
+
+			if (callback) {
+				i = listeners.indexOf(callback);
+				if (i !== -1) listeners.splice(i, 1);
+			} else {
+				_static.listeners[event] = [];
+			}
+		},
+
+		// event codes
+
+		INIT: 'init'
+	};
+
+	return _public;
+}())
---
+++ src/gf.vector.js
@@ -0,0 +1,362 @@
+;(function(GF){'use strict';
+	
+	// TODO matrix operations
+	// TODO quaternions
+	// TODO gf.matrixstack.js
+
+	if (typeof GF === 'undefined') throw new TypeError('GF is undefined - load gf.core first');
+
+	var VectorArray = Float32Array,
+		
+		// prototype superclass definitions
+		
+		prototypes = {
+			Vec2: new VectorArray(2),
+			Vec3: new VectorArray(3),
+			Vec4: new VectorArray(4),
+			Mat3: new VectorArray(9),
+			Mat4: new VectorArray(16)
+		},
+		definitions = {
+
+			// helper definitions
+
+			Copyable: {
+				copy: { value: function() { return Object.create(this); } }
+			},
+
+			// vector defintions
+
+			// 2-component vector (x, y)
+
+			Vec2: {
+				_superclass: 'Copyable',
+				x: {
+					get: function() { return this[0]; },
+					set: function(n) { return this[0] = n; }
+				},
+				y: {
+					get: function() { return this[1]; },
+					set: function(n) { return this[1] = n; }
+				},
+				normalize: { value: function() {
+					this.magnitude = 1;
+					return this;
+				}},
+				dot: { value: function(v) {
+					return this[0] * v[0] + this[1] * v[1];
+				}},
+				add: { value: function(v) {
+					this[0] += v[0];
+					this[1] += v[1];
+
+					return this;
+				}},
+				sub: { value: function(v) {
+					this[0] -= v[0];
+					this[1] -= v[1];
+
+					return this;
+				}},
+				mul: { value: function(v) {
+					this[0] *= v[0];
+					this[1] *= v[1];
+
+					return this;
+				}},
+				div: { value: function(v) {
+					this[0] /= v[0];
+					this[1] /= v[1];
+
+					return this;
+				}},
+				scale: { value: function(x, y) {
+					this[0] *= x;
+					this[1] *= y || x;
+
+					return this;
+				}},
+				magnitude: {
+					get: function() { return Math.sqrt(this[0] * this[0] + this[1] * this[1]); },
+					set: function(n) {
+						var l = this.length / n;
+
+						this[0] /= l;
+						this[1] /= l;
+
+						return n;
+					}
+				}
+			},
+
+			// 3-component vector (x, y, z)
+
+			Vec3: {
+				_superclass: 'Vec2',
+				z: {
+					get: function() { return this[2]; },
+					set: function(n) { return this[2] = n; }
+				},
+				cross: { value: function(v, result) {
+					var r = result || this.copy();
+
+					r[0] = this[1] * v[2] - this[2] * v[1];
+					r[1] = this[2] * v[0] - this[0] * v[2];
+					r[2] = this[0] * v[1] - this[1] * v[0];
+
+					return r;					
+				}},
+				dot: { value: function(v) {
+					return this[0] * v[0] + this[1] * v[1] + this[2] * v[2];
+				}},
+				add: { value: function(v) {
+					this[0] += v[0];
+					this[1] += v[1];
+					this[2] += v[2];
+
+					return this;
+				}},
+				sub: { value: function(v) {
+					this[0] -= v[0];
+					this[1] -= v[1];
+					this[2] -= v[2];
+
+					return this;
+				}},
+				mul: { value: function(v) {
+					this[0] *= v[0];
+					this[1] *= v[1];
+					this[2] *= v[2];
+
+					return this;
+				}},
+				div: { value: function(v) {
+					this[0] /= v[0];
+					this[1] /= v[1];
+					this[2] /= v[2];
+
+					return this;
+				}},
+				scale: { value: function(x, y, z) {
+					this[0] *= x;
+					this[1] *= y || x;
+					this[2] *= z || x;
+
+					return this;
+				}},
+				magnitude: {
+					get: function() { return Math.sqrt(this[0] * this[0] + this[1] * this[1] + this[2] * this[2]); },
+					set: function(n) {
+						var l = this.magnitude / n;
+
+						this[0] /= l;
+						this[1] /= l;
+						this[2] /= l;
+
+						return n;
+					}
+				}
+			},
+
+			// 4-component vector (x, y, z, w)
+
+			// Note that for convenience in writing 3D applications, Vec4 behaves like a Vec3 with a w component.
+			// This means that vector arithmetic performed with a Vec4 will not use or modify the w component.
+			// As with Vec3, Vec4.cross only accepts 1 other parameter, which can be a Vec3 or Vec4.
+			// The default value of w is 1.
+
+			Vec4: {
+				_superclass: 'Vec3',
+				w: {
+					get: function() { return this[3]; },
+					set: function(n) { return this[3] = n; }	
+				}
+			},
+
+			// matrix definitions, column major
+
+			// 3x3 matrix
+
+			Mat3: {
+				_superclass: 'Copyable',
+				m00: {
+					get: function() { return this[0]; },
+					set: function(n) { return this[0] = n; }
+				},
+				m10: {
+					get: function() { return this[1]; },
+					set: function(n) { return this[1] = n; }
+				},
+				m20: {
+					get: function() { return this[2]; },
+					set: function(n) { return this[2] = n; }
+				},
+				m01: {
+					get: function() { return this[3]; },
+					set: function(n) { return this[3] = n; }
+				},
+				m11: {
+					get: function() { return this[4]; },
+					set: function(n) { return this[4] = n; }
+				},
+				m21: {
+					get: function() { return this[5]; },
+					set: function(n) { return this[5] = n; }
+				},
+				m02: {
+					get: function() { return this[6]; },
+					set: function(n) { return this[6] = n; }
+				},
+				m12: {
+					get: function() { return this[7]; },
+					set: function(n) { return this[7] = n; }
+				},
+				m22: {
+					get: function() { return this[8]; },
+					set: function(n) { return this[8] = n; }
+				}
+			},
+
+			// 4x4 matrix
+
+			Mat4: {
+				_superclass: 'Copyable',
+				m00: {
+					get: function() { return this[0]; },
+					set: function(n) { return this[0] = n; }
+				},
+				m10: {
+					get: function() { return this[1]; },
+					set: function(n) { return this[1] = n; }
+				},
+				m20: {
+					get: function() { return this[2]; },
+					set: function(n) { return this[2] = n; }
+				},
+				m30: {
+					get: function() { return this[3]; },
+					set: function(n) { return this[3] = n; }
+				},
+				m01: {
+					get: function() { return this[4]; },
+					set: function(n) { return this[4] = n; }
+				},
+				m11: {
+					get: function() { return this[5]; },
+					set: function(n) { return this[5] = n; }
+				},
+				m21: {
+					get: function() { return this[6]; },
+					set: function(n) { return this[6] = n; }
+				},
+				m31: {
+					get: function() { return this[7]; },
+					set: function(n) { return this[7] = n; }
+				},
+				m02: {
+					get: function() { return this[8]; },
+					set: function(n) { return this[8] = n; }
+				},
+				m12: {
+					get: function() { return this[9]; },
+					set: function(n) { return this[9] = n; }
+				},
+				m22: {
+					get: function() { return this[10]; },
+					set: function(n) { return this[10] = n; }
+				},
+				m32: {
+					get: function() { return this[11]; },
+					set: function(n) { return this[11] = n; }
+				},
+				m03: {
+					get: function() { return this[12]; },
+					set: function(n) { return this[12] = n; }
+				},
+				m13: {
+					get: function() { return this[13]; },
+					set: function(n) { return this[13] = n; }
+				},
+				m23: {
+					get: function() { return this[14]; },
+					set: function(n) { return this[14] = n; }
+				},
+				m33: {
+					get: function() { return this[15]; },
+					set: function(n) { return this[15] = n; }
+				}
+			},
+		},
+
+		// looping indices
+		prototypeName,
+		prototype,
+		definition;
+
+	// assemble prototypes
+
+	for (prototypeName in definitions) {
+		prototype = prototypes[prototypeName] || (prototypes[prototypeName] = {});
+		definition = definitions[prototypeName];
+
+		if (typeof definition._superclass === 'string' && typeof definitions[definition._superclass] === 'object') {
+			// compose properties from defined superclass
+			defineProperties(prototype, definitions[definition._superclass], prototypeName);
+		}
+
+		defineProperties(prototype, definition);
+	}
+
+	function defineProperties(prototype, definition, subclass) {
+		var propertyName,
+			property,
+			subclassDefinition = subclass && definitions[subclass];
+
+		for (propertyName in definition) {
+			if (propertyName === '_superclass') continue;
+
+			property = definition[propertyName];
+
+			if (subclassDefinition) {
+				// this is a superclass, add properties to subclass that it doesn't override
+				subclassDefinition[propertyName] = subclassDefinition[propertyName] || property;
+			} else {
+				// this is a subclass, define the property
+				Object.defineProperty(prototype, propertyName, property);
+			}
+		}	
+	}
+
+	// create constructors
+
+	GF.Vec2 = function(x, y) {
+		var v = Object.create(prototypes.Vec2);
+
+		v[0] = x || 0;
+		v[1] = y || 0;
+
+		return v;
+	};
+
+	GF.Vec3 = function(x, y, z) {
+		var v = Object.create(prototypes.Vec3);
+
+		v[0] = x || 0;
+		v[1] = y || 0;
+		v[2] = z || 0;
+
+		return v;
+	};
+
+	GF.Vec4 = function(x, y, z, w) {
+		var v = Object.create(prototypes.Vec4);
+
+		v[0] = x || 0;
+		v[1] = y || 0;
+		v[2] = z || 0;
+		v[3] = w || 1;
+
+		return v;
+	};
+
+}(window.GF));
